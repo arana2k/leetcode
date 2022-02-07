30558e71026eb0d0ae3794b3183325a6c0fe6b42
Abhishek Rana <55630076+abhishek8778@users.noreply.github.com>
2022-02-07 22:03:09 +0530

    Create day4.cpp

---
+++ feb-challenge/day4.cpp
@@ -0,0 +1,19 @@
+// DAY 4 (525. Contiguous Array)======================================================================
+
+int findMaxLength(vector<int> &nums)
+{
+    unordered_map<int, int> mp;
+    mp[0] = -1;
+    int pol = 0;
+    int ans = 0;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        pol += (nums[i] == 1);
+        pol -= (nums[i] == 0);
+        if (!mp.count(pol))
+            mp[pol] = i;
+        else
+            ans = max(ans, i - mp[pol]);
+    }
+    return ans;
+}
