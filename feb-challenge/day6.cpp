abe02117e670e0f328fa82347a66d4744218349e
Abhishek Rana <55630076+abhishek8778@users.noreply.github.com>
2022-02-07 22:04:16 +0530

    Create day6.cpp

---
+++ feb-challenge/day6.cpp
@@ -0,0 +1,14 @@
+// DAY 6 (80. Remove Duplicates from Sorted Array II)===========================================================================
+
+int removeDuplicates(vector<int> &nums)
+{
+    unordered_map<int, int> m;
+    int i = 0, j = 0;
+    while (i < nums.size())
+    {
+        if (++m[nums[i]] <= 2)
+            swap(nums[i], nums[j++]);
+        i++;
+    }
+    return j;
+}
