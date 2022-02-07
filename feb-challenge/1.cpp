b44812f927e0a0bc7f7fdcbf0980a55f082c00a0
Abhishek Rana <55630076+abhishek8778@users.noreply.github.com>
2022-02-07 22:00:16 +0530

    Update 1.cpp

--- feb-challenge/1.cpp
+++ feb-challenge/1.cpp
@@ -1,1 +1,12 @@
+// DAY 1 (121. Best Time to Buy and Sell Stock)================================================================================
 
+int maxProfit(vector<int> &prices)
+{
+    int mn = INT_MAX, ans = 0;
+    for (int x : prices)
+    {
+        ans = max(ans, x - mn);
+        mn = min(mn, x);
+    }
+    return ans;
+}
