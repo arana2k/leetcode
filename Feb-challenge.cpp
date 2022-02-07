dcc95041fb3bc0ab52d850d1075af36a5032470e
Abhishek Rana <55630076+abhishek8778@users.noreply.github.com>
2022-02-07 18:16:43 +0530

    Update Feb-challenge.cpp

--- Feb-challenge.cpp
+++ Feb-challenge.cpp
@@ -199,3 +199,15 @@ int removeDuplicates(vector<int> &nums)
     }
     return j;
 }
+
+// DAY 7(389. Find the Difference)=============================================================================================
+
+//METHOD(BITWISE OPERATION (USE OF XOR))
+
+char findTheDifference(string s, string t) {
+        char c = 0;
+        for(char cs: s) c ^= cs;
+        for(char ct: t) c ^= ct;
+        return c;
+    }
+};
