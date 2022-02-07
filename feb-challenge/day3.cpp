91e5918f132871378786dc1efd0a8ab8a70eedcf
Abhishek Rana <55630076+abhishek8778@users.noreply.github.com>
2022-02-07 22:02:39 +0530

    Create day3.cpp

---
+++ feb-challenge/day3.cpp
@@ -0,0 +1,17 @@
+// DAY 3 (454. 4Sum II)=========================================================================================
+
+int fourSumCount(vector<int> &nums1, vector<int> &nums2, vector<int> &nums3, vector<int> &nums4)
+{
+    int n = nums1.size();
+    unordered_map<int, int> mp;
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            mp[nums1[i] + nums2[j]]++;
+
+    int ans = 0;
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            ans += mp[-(nums3[i] + nums4[j])];
+
+    return ans;
+}
