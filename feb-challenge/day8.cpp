1f690c7e7e3e0f19da0fb4dc627b515ff72b55f2
Abhishek Rana <55630076+abhishek8778@users.noreply.github.com>
2022-02-10 15:27:02 +0530

    Update day8.cpp

--- feb-challenge/day8.cpp
+++ feb-challenge/day8.cpp
@@ -1,24 +1,61 @@
-// DAY 7(389. Find the Difference)=============================================================================================
+// DAY 8(532. K-diff Pairs in an Array)=============================================================================================
+
+//we have to find all pairs the array which have an absolute difference of 'k' and then eliminate those which are not unique.
 
 
-int findPairs(vector<int> &nums, int k) {
-        map<int, int> hash;
-        int pairs = 0, len = nums.size(), sum;
-        for (auto &n: nums) {
-            ++hash[n];
+//Using maps
+
+class Solution {
+public:
+    int findPairs(vector<int>& nums, int k) {
+        unordered_map<int,int> a;
+        for(int i:nums)
+            a[i]++;
+        int ans=0;
+        for(auto x:a)
+        {
+            if(k==0)
+            {    
+                if(x.second>1)
+                ans++;
+            }
+             else if(a.find(x.first+k)!=a.end())
+                ans++;
         }
+        
+        return ans;
+    }
+};
 
-        for (int i = 0; i < len; ++i) {
-            sum = (nums[i] + k);
-            if (hash[sum] > 0) {
-                if (sum == nums[i]) {
-                    pairs = (hash[sum] > 1) ? pairs + 1 : pairs;
-                } else {
-                    ++pairs;
+//using two pointer
+
+class Solution {
+public:
+    int findPairs(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        int ans=0,i=0,j=1;
+        for(i,j;i<nums.size() and j<nums.size();)
+        {
+            if(i==j or nums[j]-nums[i]<k)
+                j++;
+            else 
+            {
+                if(nums[j]-nums[i]==k)
+                {
+                    ans++;
+                    j++;
+                    for(;j<nums.size();j++)
+                        if(nums[j]!=nums[j-1])
+                            break;
+                    if(j==nums.size())
+                    return ans;
+                    j--;                  
                 }
-                hash[sum] = 0;
+                i++;
+                while(i<j and nums[i]==nums[i-1])
+                    i++;
             }
         }
-
-        return pairs;
+        return ans;
     }
+};
