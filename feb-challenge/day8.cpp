318c6cbdb3ca7145cadceb6378dd05a9fc535714
Abhishek Rana <55630076+abhishek8778@users.noreply.github.com>
2022-02-11 15:05:58 +0530

    Create day8.cpp

---
+++ feb-challenge/day8.cpp
@@ -0,0 +1,41 @@
+// DAY 8(258. Add Digits)=============================================================================================
+
+//RECURSION :
+
+
+class Solution {
+public:
+    int addDigits(int num) {
+      int sum=0;
+        while(num)
+        {
+            sum+=(num%10);
+            num/=10;
+        }
+        if(sum<10)
+            return sum;
+        else
+            return addDigits(sum);
+    }
+};
+
+//ITERATION :
+
+
+class Solution {
+public:
+    int addDigits(int num) {
+      int sum=0;
+        while(num>9)
+        {
+            while(num)
+            {
+                sum+=(num%10);
+                num/=10;
+            }
+            num=sum;
+            sum=0;
+        }
+        return num;
+    }
+};
