3517959bcc9a4d5e06cdc9e4c7fe81d4399526ed
Abhishek Rana <55630076+abhishek8778@users.noreply.github.com>
2022-02-13 21:27:21 +0530

    Create day13.cpp

---
+++ feb-challenge/day13.cpp
@@ -0,0 +1,25 @@
+
+
+
+
+
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        
+         vector<vector<int>> res(1);
+        sort(nums.begin(), nums.end());
+        for(int i = 0; i < nums.size(); i++)
+        {
+            int size = res.size();
+            for(int j = 0; j < size; j++)
+            {
+                res.push_back(res[j]);
+                res.back().push_back(nums[i]);
+            }
+        }
+        
+        return res;
+        
+    }
+};
