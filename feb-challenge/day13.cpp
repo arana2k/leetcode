38d37cc9c2c92c6db54f54e8d689eb4cb816a5a7
Abhishek Rana <55630076+abhishek8778@users.noreply.github.com>
2022-02-13 21:31:56 +0530

    Update day13.cpp

--- feb-challenge/day13.cpp
+++ feb-challenge/day13.cpp
@@ -1,25 +1,61 @@
-// DAY 12(78. Subsets)=============================================================================================
+// DAY 13(78. Subsets)=============================================================================================
+
+//Recursive (Backtracking)
+
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> subs;
+        vector<int> sub;
+        subsets(nums, 0, sub, subs);
+        return subs;
+    }
+private:
+    void subsets(vector<int>& nums, int i, vector<int>& sub, vector<vector<int>>& subs) {
+        subs.push_back(sub);
+        for (int j = i; j < nums.size(); j++) {
+            sub.push_back(nums[j]);
+            subsets(nums, j + 1, sub, subs);
+            sub.pop_back();
+        }
+    }
+};
 
 
+//Iterative
 
 
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        
-         vector<vector<int>> res(1);
-        sort(nums.begin(), nums.end());
-        for(int i = 0; i < nums.size(); i++)
-        {
-            int size = res.size();
-            for(int j = 0; j < size; j++)
-            {
-                res.push_back(res[j]);
-                res.back().push_back(nums[i]);
+        vector<vector<int>> subs = {{}};
+        for (int num : nums) {
+            int n = subs.size();
+            for (int i = 0; i < n; i++) {
+                subs.push_back(subs[i]); 
+                subs.back().push_back(num);
             }
         }
-        
-        return res;
-        
+        return subs;
     }
 };
+
+
+//Bit Manipulation
+
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        int n = nums.size(), p = 1 << n;
+        vector<vector<int>> subs(p);
+        for (int i = 0; i < p; i++) {
+            for (int j = 0; j < n; j++) {
+                if ((i >> j) & 1) {
+                    subs[i].push_back(nums[j]);
+                }
+            }
+        }
+        return subs;
+    }
+};
+
