3228bdebcc7d1350863efa1e5d45f4d3a93dd188
Abhishek Rana <55630076+abhishek8778@users.noreply.github.com>
2022-02-13 21:31:39 +0530

    Update Feb-challenge.cpp

--- Feb-challenge.cpp
+++ Feb-challenge.cpp
@@ -388,4 +388,103 @@ public:
     }
 };
 
+// DAY 12(127. Word Ladder)=============================================================================================
+
+
+
+
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        unordered_set<string> dict(wordList.begin(), wordList.end());
+        queue<string> todo;
+        todo.push(beginWord);
+        int ladder = 1;
+        while (!todo.empty()) {
+            int n = todo.size();
+            for (int i = 0; i < n; i++) {
+                string word = todo.front();
+                todo.pop();
+                if (word == endWord) {
+                    return ladder;
+                }
+                dict.erase(word);
+                for (int j = 0; j < word.size(); j++) {
+                    char c = word[j];
+                    for (int k = 0; k < 26; k++) {
+                        word[j] = 'a' + k;
+                        if (dict.find(word) != dict.end()) {
+                            todo.push(word);
+                        }
+                     }
+                    word[j] = c;
+                }
+            }
+            ladder++;
+        }
+        return 0;
+    }
+};
+
+
+// DAY 13(78. Subsets)=============================================================================================
+
+//Recursive (Backtracking)
+
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> subs;
+        vector<int> sub;
+        subsets(nums, 0, sub, subs);
+        return subs;
+    }
+private:
+    void subsets(vector<int>& nums, int i, vector<int>& sub, vector<vector<int>>& subs) {
+        subs.push_back(sub);
+        for (int j = i; j < nums.size(); j++) {
+            sub.push_back(nums[j]);
+            subsets(nums, j + 1, sub, subs);
+            sub.pop_back();
+        }
+    }
+};
+
+
+//Iterative
+
+
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> subs = {{}};
+        for (int num : nums) {
+            int n = subs.size();
+            for (int i = 0; i < n; i++) {
+                subs.push_back(subs[i]); 
+                subs.back().push_back(num);
+            }
+        }
+        return subs;
+    }
+};
+
+
+//Bit Manipulation
+
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        int n = nums.size(), p = 1 << n;
+        vector<vector<int>> subs(p);
+        for (int i = 0; i < p; i++) {
+            for (int j = 0; j < n; j++) {
+                if ((i >> j) & 1) {
+                    subs[i].push_back(nums[j]);
+                }
+            }
+        }
+        return subs;
+    }
+};
 
