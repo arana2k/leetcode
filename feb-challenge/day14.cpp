5e0d6d1a6fb3c64be1c2c2e816dedf6541e37cc3
Abhishek Rana <55630076+abhishek8778@users.noreply.github.com>
2022-02-15 23:28:14 +0530

    Create day14.cpp

---
+++ feb-challenge/day14.cpp
@@ -0,0 +1,40 @@
+// DAY 14(104. Maximum Depth of Binary Tree)=============================================================================================
+
+
+//Depth-first-search
+
+int maxDepth(TreeNode* root) {
+        if(!root) return 0;
+        int maxLeft = maxDepth(root->left);
+        int maxRight = maxDepth(root->right);
+        return max(maxLeft, maxRight)+1;
+    }
+
+
+
+//Breadth-first-search
+int maxDepth(TreeNode *root)
+{
+    if(root == NULL)
+        return 0;
+    
+    int res = 0;
+    queue<TreeNode *> q;
+    q.push(root);
+    while(!q.empty())
+    {
+        ++ res;
+        for(int i = 0, n = q.size(); i < n; ++ i)
+        {
+            TreeNode *p = q.front();
+            q.pop();
+            
+            if(p -> left != NULL)
+                q.push(p -> left);
+            if(p -> right != NULL)
+                q.push(p -> right);
+        }
+    }
+    
+    return res;
+}
