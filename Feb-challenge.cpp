8e39a5a90bc1f2ae9db2d434b34796ce958c8b61
Abhishek Rana <55630076+abhishek8778@users.noreply.github.com>
2022-02-15 23:32:50 +0530

    Update Feb-challenge.cpp

--- Feb-challenge.cpp
+++ Feb-challenge.cpp
@@ -488,3 +488,90 @@ public:
     }
 };
 
+// DAY 14(104. Maximum Depth of Binary Tree)=============================================================================================
+
+
+//Depth-first-search
+
+int maxDepth(TreeNode* root) {
+        if(!root) return 0;
+        int maxLeft = maxDepth(root->left);
+        int maxRight = maxDepth(root->right);
+        return max(maxLeft, maxRight)+1;
+    }
+
+
+
+//Breadth-first-search
+int maxDepth(TreeNode *root)
+{
+    if(root == NULL)
+        return 0;
+    
+    int res = 0;
+    queue<TreeNode *> q;
+    q.push(root);
+    while(!q.empty())
+    {
+        ++ res;
+        for(int i = 0, n = q.size(); i < n; ++ i)
+        {
+            TreeNode *p = q.front();
+            q.pop();
+            
+            if(p -> left != NULL)
+                q.push(p -> left);
+            if(p -> right != NULL)
+                q.push(p -> right);
+        }
+    }
+    
+    return res;
+}
+
+// DAY 15(136. Single Number)=============================================================================================
+
+//USING MAPS (NOT USING CONSTANT SPACE )
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) { 
+       unordered_map<int,int> a;
+	   for(auto x: nums)
+		   a[x]++;
+	   for(auto z:a)
+		   if(z.second==1)
+			   return z.first;
+	   return -1;
+    }
+}
+
+
+//USING SORTING (USING CONSTANT SPACE )
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) { 
+       sort(nums.begin(),nums.end());
+        for(int i=1;i<nums.size();i+=2)
+        {
+            if(nums[i]!=nums[i-1])
+                return nums[i-1];
+        }
+        return nums[nums.size()-1];
+    }
+}
+
+//using xor method
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) { 
+       int ans=0;
+	   for(auto x:nums)
+	   ans^=x;
+	   return ans;
+    }
+};
+
+
