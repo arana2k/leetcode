ad93a0bcb8eda13351c3e218c285451514de04dc
Abhishek Rana <55630076+abhishek8778@users.noreply.github.com>
2022-02-15 23:32:41 +0530

    136. Single Number

---
+++ feb-challenge/day15.cpp
@@ -0,0 +1,44 @@
+// DAY 15(136. Single Number)=============================================================================================
+
+//USING MAPS (NOT USING CONSTANT SPACE )
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) { 
+       unordered_map<int,int> a;
+	   for(auto x: nums)
+		   a[x]++;
+	   for(auto z:a)
+		   if(z.second==1)
+			   return z.first;
+	   return -1;
+    }
+}
+
+
+//USING SORTING (USING CONSTANT SPACE )
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) { 
+       sort(nums.begin(),nums.end());
+        for(int i=1;i<nums.size();i+=2)
+        {
+            if(nums[i]!=nums[i-1])
+                return nums[i-1];
+        }
+        return nums[nums.size()-1];
+    }
+}
+
+//using xor method
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) { 
+       int ans=0;
+	   for(auto x:nums)
+	   ans^=x;
+	   return ans;
+    }
+};
