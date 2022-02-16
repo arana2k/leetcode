48c738d1a97d387bc9e4c09b2515d846050a3822
Abhishek Rana <55630076+abhishek8778@users.noreply.github.com>
2022-02-16 16:21:06 +0530

    Create day16.cpp

---
+++ feb-challenge/day16.cpp
@@ -0,0 +1,16 @@
+// DAY 16(24. Swap Nodes in Pairs)=============================================================================================
+
+
+  ListNode* swapPairs(ListNode* head) {
+        if (head == nullptr || head->next == nullptr) return head;
+        // 2 is new head, 1 is head
+        ListNode* new_head = head->next;
+        // store 3
+        ListNode* third = head->next->next;
+        // 2->1
+        new_head->next = head;
+        // 1->3
+        head->next = swapPairs(third);
+        
+        return new_head;
+    }
