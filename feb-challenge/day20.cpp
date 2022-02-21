50de38461ddfe00169ecd722d1c347add091a14f
Abhishek Rana <55630076+abhishek8778@users.noreply.github.com>
2022-02-21 23:03:21 +0530

    Create day20.cpp

---
+++ feb-challenge/day20.cpp
@@ -0,0 +1,73 @@
+
+// DAY 20(1288. Remove Covered Intervals)=============================================================================================
+
+
+
+/*
+
+1.The interval [a, b) is covered by the interval [c, d) if and only if c <= a and b <= d.
+	eg Interval [3,6] is covered by [2,8], therefore it should be removed.
+	
+	Let see the Number line :)
+	
+	                                           1  2  3  4  5  6  7  8
+											   1-------4
+											         3--------6
+												  2-----------------8
+	clearly we can see that [3 6] is covered by [2,8] and therefore it should be removed.
+
+2. We will Sort the vector in ascending order to get this type of arrangement.
+		//e.g. (1,5), (1,8), (2,9), (3,5), (4,7), (4,9)
+		
+3. For finding the remaining interaval, ifa[1][0] && a[1][1] both greater than a[0][0] && a[0][1],
+this means the previous interval is not covered by the next one, therefore we will increase the count.
+			consider the case [[1,3],[2,4],[4,8]]
+								1   2  3  4  5  6  7  8
+								1------3
+									2-----4
+									      4-----------8
+											 
+			No interval is completely overlaped by other therefore remainig interaval are 3.
+			
+			how answer is 3 , at first cnt is initialised to 1
+			now a[0,0] i.e 1 and a[1,0] i.e 2   1 < 2 also,
+			    a[0,1] i.e 3 and a[1,1] i.e 4   3 < 4    , therefore cnt is incremented by 2 now,
+					
+					also a[2,0] and a[2,1] satisy same condition with a[1,0] and a[1,1] , cnt becomes 3
+					
+					
+					
+					*/
+
+class Solution {
+public:
+    int removeCoveredIntervals(vector<vector<int>>& intervals) {
+        
+        // sorting the intervals(vector)
+        sort(intervals.begin(),intervals.end());        
+           
+        int x1 = intervals[0][0];
+        int x2 = intervals[0][1];
+        
+		 int res = 1;  //one for x1 and x2;
+		
+		// ifa[i][0] && a[i][1] both greater than a[i-1][0] && a[i-1][1]
+		// increase the cnt.
+        for(int i= 1; i<intervals.size(); ++i)
+        {
+            if(intervals[i][0] > x1 && intervals[i][1] > x2)
+                ++res;
+            
+			// updating x1 & x2 with next intervals
+			as we are comparing from upcoming ones.
+            if(intervals[i][1] > x2)
+            {
+                x1 = intervals[i][0];
+                x2 = intervals [i][1];
+            }
+        }
+        
+        return res;       // return cnt
+    }
+
+};
