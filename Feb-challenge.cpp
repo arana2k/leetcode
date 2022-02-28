9c10a4365cbe9512eaf49e99dc20adc5ce2342c0
Abhishek Rana <55630076+abhishek8778@users.noreply.github.com>
2022-02-28 09:51:33 +0530

    Update Feb-challenge.cpp

--- Feb-challenge.cpp
+++ Feb-challenge.cpp
@@ -1105,5 +1105,103 @@ public:
 }
 };
 
+// DAY 27 (662. Maximum Width of Binary Tree)==============================================================================
+
+
+class Solution {
+public:
+    int widthOfBinaryTree(TreeNode* root) {
+        if(root == NULL)
+            return 0;
+        
+        int res = 1;
+        queue<pair<TreeNode*, int>> q;
+        
+        // I am using intialising list
+        q.push({root, 0});      // also can use make_pair
+        
+        while(!q.empty())
+        {
+            int cnt = q.size();
+            // start is the index of root node for first level
+            int start = q.front().second;
+            int end = q.back().second;
+            
+            res = max(res,end-start + 1);
+            
+            for(int i = 0; i <cnt; ++i)
+            {
+                pair<TreeNode*, int> p = q.front();
+                // we will use it while inserting it children
+                // left child will be 2 * idx + 1;
+                // right chils will be 2 * idx + 2;
+                int idx = p.second - start;
+                
+                q.pop();
+                
+                // if  left child exist
+                if(p.first->left != NULL)
+                    q.push({p.first->left, (long long)2 * idx + 1});
+                
+                // if right child exist
+                if(p.first->right != NULL)
+                    q.push({p.first->right, (long long) 2 * idx + 2});
+            }
+        }
+        
+        return res;
+        
+    }
+};
+
+
+
+
+// DAY 28 (228. Summary Ranges)==============================================================================
+
+
+
+
+class Solution {
+public:
+    vector<string> summaryRanges(vector<int>& nums) {
+		// resultant string
+        vector<string> result;
+        
+        int n = nums.size();
+		// if size happens to be  zero return empty string
+        if(n == 0 )
+                return result;
+        
+		// assigning first element to a
+        int a = nums[0];
+        
+        for(int i = 0; i<n; i++)
+        {
+			// if one of both is true
+            if( i == n-1 || nums[i]+1 != nums[i+1])
+            {
+			    // if current element is not equals a
+				// this means we have found a range.
+                if(nums[i] != a)
+                    result.push_back(to_string(a)+ "->"+ to_string(nums[i]));
+					
+				// this means we have reached to the end of string and now
+				// we have to add a that should be the last element
+                else
+                        result.push_back(to_string(a));
+						
+				// checking  for this condition so that a got updated for next range
+				// also n-1 so that a doesn't contain out of bound value
+                if(i != n-1)
+                    a = nums[i+1];
+            }
+        }
+		// return result
+        return result;
+    }
+};
+
+
 
 
